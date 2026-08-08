48b53475e0e308c1ab034a7648bc6c4d3020913e
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/github_it.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>ConfigWidget</name>
+    <message>
+        <source>Saved searches</source>
+        <translation>Ricerche salvate</translation>
+    </message>
+    <message>
+        <source>See the GitHub [user](%1), [repo](%2) and [issue](%3) search documentation.</source>
+        <translation>Consulta la documentazione di ricerca GitHub [utente](%1), [repo](%2) e [issue](%3).</translation>
+    </message>
+    <message>
+        <source>GitHub OAuth app credentials (See README)</source>
+        <translation>Credenziali app OAuth GitHub (vedi README)</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Titolo</translation>
+    </message>
+    <message>
+        <source>Query</source>
+        <translation>Query</translation>
+    </message>
+    <message>
+        <source>New search</source>
+        <translation>Nuova ricerca</translation>
+    </message>
+</context>
+<context>
+    <name>GitHubItem</name>
+    <message>
+        <source>Show on GitHub</source>
+        <translation>Mostra su GitHub</translation>
+    </message>
+    <message>
+        <source>Open issues</source>
+        <translation>Issue aperte</translation>
+    </message>
+    <message>
+        <source>Open pull requests</source>
+        <translation>Pull request aperte</translation>
+    </message>
+    <message>
+        <source>Open discussions</source>
+        <translation>Discussioni aperte</translation>
+    </message>
+    <message>
+        <source>Open wiki</source>
+        <translation>Apri wiki</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Assigned issues</source>
+        <translation>Issue assegnate</translation>
+    </message>
+    <message>
+        <source>Created issues</source>
+        <translation>Issue create</translation>
+    </message>
+    <message>
+        <source>Mentions</source>
+        <translation>Menzioni</translation>
+    </message>
+    <message>
+        <source>Recent activity</source>
+        <translation>Attività recente</translation>
+    </message>
+    <message>
+        <source>GitHub users</source>
+        <translation>Utenti GitHub</translation>
+    </message>
+    <message>
+        <source>Search GitHub users</source>
+        <translation>Cerca utenti GitHub</translation>
+    </message>
+    <message>
+        <source>GitHub repositories</source>
+        <translation>Repository GitHub</translation>
+    </message>
+    <message>
+        <source>Search GitHub repositories</source>
+        <translation>Cerca repository GitHub</translation>
+    </message>
+    <message>
+        <source>My repositories</source>
+        <translation>I miei repository</translation>
+    </message>
+    <message>
+        <source>Albert repositories</source>
+        <translation>Repository Albert</translation>
+    </message>
+    <message>
+        <source>Archived Albert repositories</source>
+        <translation>Repository Albert archiviati</translation>
+    </message>
+    <message>
+        <source>GitHub issues</source>
+        <translation>Issue GitHub</translation>
+    </message>
+    <message>
+        <source>Search GitHub issues</source>
+        <translation>Cerca issue GitHub</translation>
+    </message>
+    <message>
+        <source>Assigned pull requests</source>
+        <translation>Pull request assegnate</translation>
+    </message>
+    <message>
+        <source>Created pull requests</source>
+        <translation>Pull request create</translation>
+    </message>
+    <message>
+        <source>Review requests</source>
+        <translation>Richieste di revisione</translation>
+    </message>
+    <message>
+        <source>Albert issues</source>
+        <translation>Issue Albert</translation>
+    </message>
+    <message>
+        <source>Albert pull requests</source>
+        <translation>Pull request Albert</translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation>Mostra</translation>
+    </message>
+    <message>
+        <source>Show on GitHub</source>
+        <translation>Mostra su GitHub</translation>
+    </message>
+</context>
+</TS>
